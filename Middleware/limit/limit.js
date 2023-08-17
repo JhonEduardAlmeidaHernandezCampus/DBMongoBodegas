51b2f712fd643caf2604b7b7f4e11264a56318c6
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-16 22:14:51 -0500

    feat: :sparkles: Limit
    Creacion del archivo de limite de peticiones y tiempo
    
    by: Jhon Hernandez

---
+++ Middleware/limit/limit.js
@@ -0,0 +1,25 @@
+import { rateLimit } from "express-rate-limit";
+
+export let limit = () => {
+    return rateLimit({
+        windowMs: 10 * 1000,
+        max: 5,
+        standardHeaders: true,
+        legacyHeaders: false,
+        skip: (req, res) => {
+            if(req.headers["content-length"] > 500 ){
+                res.status(413).send({
+                    status: 413,
+                    message: "El tamaño es incorrecto"
+                });
+                return true;
+            }
+        },
+        message: (req, res) => {
+            res.status(429).send({
+                status: 429,
+                message: "Pailas, muchos intentos, vayase a dormir"
+            })
+        }
+    })
+}
